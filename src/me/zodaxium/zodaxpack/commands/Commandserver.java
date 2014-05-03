39c704428f01933617e88496cf5e0dbdcc3b6d6c
TheInfoCoder <dylanmcpeek12@gmail.com>
2014-05-03 12:09:21 -0500

    Changed code to use central hashmap

--- src/me/zodaxium/zodaxpack/commands/Commandserver.java
+++ src/me/zodaxium/zodaxpack/commands/Commandserver.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import me.zodaxium.zodaxpack.ZodaxPack;
 import net.md_5.bungee.api.ChatColor;
@@ -25,14 +24,14 @@ public class Commandserver extends Command{
 		plugin.getProxy().getPluginManager().registerCommand(plugin, this);
 	}
 	
+	@SuppressWarnings("deprecation")
 	public void execute(CommandSender sender, String[] args){
 		if(!(sender instanceof ProxiedPlayer)){ return; }
 		ProxiedPlayer p = (ProxiedPlayer) sender;
-		Map<String, ServerInfo> servers = ProxyServer.getInstance().getServers();
 		if(args.length == 0){
 		    p.sendMessage(new TextComponent(ChatColor.BLUE + "You are connected to: " + ChatColor.GOLD + ProxyServer.getInstance().getPlayer(p.getName()).getServer().getInfo().getName().substring(0, 1).toUpperCase() + ProxyServer.getInstance().getPlayer(p.getName()).getServer().getInfo().getName().substring(1)));
 		    List<String> servernames = new ArrayList<String>();
-		    for(ServerInfo server : servers.values()){
+		    for(ServerInfo server : plugin.servers.values()){
 		    	 servernames.add(server.getName().substring(0, 1).toUpperCase() + server.getName().substring(1));
 		    }
 		    Collections.sort(servernames);
@@ -44,9 +43,9 @@ public class Commandserver extends Command{
 		    	}
 		    }
 		    String message = plugin.colorize("&9Servers: &6" + sb.toString());
-		    p.sendMessage(new TextComponent(message));
+		    p.sendMessage(message);
 		}else{
-			ServerInfo server = (ServerInfo) servers.get(args[0]);
+			ServerInfo server = (ServerInfo) plugin.servers.get(args[0]);
 			if(server == null){ p.sendMessage(new TextComponent(ProxyServer.getInstance().getTranslation("no_server", new Object[0]))); }
 			else if(!server.canAccess(p)){ p.sendMessage(new TextComponent(ProxyServer.getInstance().getTranslation("no_server_permission", new Object[0]))); }
 			else{ p.connect(server); }
